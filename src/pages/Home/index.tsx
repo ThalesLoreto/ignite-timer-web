7becd1e5e31d2ec99855525207480c426b698da3
ThalesLoreto <tloreto.dev@gmail.com>
2022-07-20 16:02:34 -0300

    validating schema with zod & integrate it with react hook form

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,7 @@
 import { Play } from 'phosphor-react'
 import { SubmitHandler, useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { z } from 'zod'
 
 import {
   CountdownContainer,
@@ -11,15 +13,23 @@ import {
   TaskInput,
 } from './styles'
 
-interface Inputs {
-  task: string
-  minutesAmount: number
-}
+const newCycleFormValidationSchema = z.object({
+  task: z.string().min(1, { message: 'Informe a tarefa' }),
+  minutesAmount: z.number().min(5).max(60),
+})
+
+type Inputs = z.infer<typeof newCycleFormValidationSchema>
 
 export function Home() {
-  const { register, handleSubmit, watch } = useForm<Inputs>()
+  const { register, handleSubmit, watch, reset } = useForm<Inputs>({
+    resolver: zodResolver(newCycleFormValidationSchema),
+    defaultValues: {
+      task: '',
+      minutesAmount: 0,
+    },
+  })
 
-  const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data)
+  const onSubmit: SubmitHandler<Inputs> = (data) => reset()
 
   const task = watch('task')
   const isSubmitDisabled = !task
